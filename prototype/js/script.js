becfe42f318e55f3cc41670610239ac4ac2f8324
m-parducci <parduccimari@hotmail.com>
2023-12-10 00:23:19 -0300

    btao concluir

--- prototype/js/script.js
+++ prototype/js/script.js
@@ -112,14 +112,14 @@ submitButton.addEventListener('click', () => {
 //script navegacao bottom menu 
 function navegarPara(pagina) {
     window.location.replace(pagina);
-  }
+}
 
 
 function trocarPagina(url) {
     window.location.href = url;
-  }
+}
 
-    //script inputValormeta
+//script inputValormeta
 function formatarMoeda(input) {
     let inputValue = input.value;
 
@@ -133,3 +133,39 @@ function formatarMoeda(input) {
     input.value = inputValue;
 }
 
+
+
+//script para mensagem de concluido metas
+function showMessage() {
+    originalValues = {}; // Initialize originalValues
+
+    alert("Concluído!"); // You can use other methods to display the message
+    const originalPlaceholders = {
+        'inputValorMeta': 'R$0,00',
+        'valor-meta': 'Valor inicial',
+        'nome-meta': 'Nome da meta',
+        'data-meta': 'Data final',
+        'descricao-meta': 'Descrição'
+    };
+
+    function clearData(...elementIds) {
+        elementIds.forEach(elementId => {
+            const element = document.getElementById(elementId);
+            if (element) {
+                // Store the original value before clearing
+                originalValues[elementId] = element.value;
+
+                // Assign placeholder value before using it
+                originalPlaceholders[elementId] = element.placeholder;
+
+                // Clear the value and set the placeholder back to the original value
+                element.value = '';
+                element.placeholder = originalPlaceholders[elementId];
+            }
+        });
+    }
+
+
+    // Clear the data for multiple input fields
+    clearData('inputValorMeta','valor-meta', 'nome-meta', 'data-meta', 'descricao-meta');
+}
