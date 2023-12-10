b3e6dceceabb8b848f5a712131b7e14a3a5c1805
m-parducci <parduccimari@hotmail.com>
2023-12-10 17:06:55 -0300

    script atualizado

--- prototype/pages/add-transacao.js
+++ prototype/pages/add-transacao.js
@@ -1,22 +1,37 @@
-// voltar paginas
+// sctipt voltar paginas
 function trocarPagina(url) {
     window.location.href = url;
 }
 
-//sharing list check all
-        const checkAllCheckbox = document.getElementById('check-all');
-        const checkItemCheckboxes = document.querySelectorAll('.check-item');
+//script inputValormeta
+function formatarMoeda(input) {
+    let inputValue = input.value;
 
-        checkAllCheckbox.addEventListener('change', () => {
-          const checked = checkAllCheckbox.checked;
-          checkItemCheckboxes.forEach(checkbox => {
-            checkbox.checked = checked;
-          });
-        });
+    // Remove caracteres não numéricos
+    inputValue = inputValue.replace(/[^0-9]/g, '');
+
+    // Converte para o formato de moeda
+    inputValue = (Number(inputValue) / 100).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+
+    // Define o valor formatado de volta no campo de input
+    input.value = inputValue;
+}
 
 
-//recorrente
+//script sharing list check all
+const checkAllCheckbox = document.getElementById('check-all');
+const checkItemCheckboxes = document.querySelectorAll('.check-item');
 
+checkAllCheckbox.addEventListener('change', () => {
+    const checked = checkAllCheckbox.checked;
+    checkItemCheckboxes.forEach(checkbox => {
+        checkbox.checked = checked;
+    });
+});
+
+
+
+//scipt recorrente-opcoes
 const inputRecorrente = document.getElementById('inputRecorrente');
 const recurrentOptions = document.querySelector('.recorrente-options');
 const customDateInput = document.getElementById('customDate');
@@ -36,15 +51,12 @@ function toggleOptions() {
     }
 }
 
-// Adicione um ouvinte de mudança ao checkbox
 inputRecorrente.addEventListener('change', toggleOptions);
 
-// Adicione um ouvinte de mudança à seleção de frequência
 frequencySelect.addEventListener('change', function () {
     customDateInput.readOnly = this.value === 'daily';
 });
 
-// Chame a função inicialmente para garantir a configuração correta na inicialização
 toggleOptions();
 
 const resetButtonForm = document.getElementById('resetButton');
@@ -66,3 +78,66 @@ resetButtonForm.addEventListener('click', () => {
     customDateInput.readOnly = false;
     recurrentOptions.style.display = 'none';
 });
+
+
+//script para mensagem de concluido transacoes
+function showMessage() {
+    // Inicializa originalValues
+    originalValues = {};
+
+    alert("Concluído!"); // Você pode usar outros métodos para exibir a mensagem
+    const originalPlaceholders = {
+        'inputValorTransacao': 'R$0,00',
+        'inputPerfil': 'Selecione',
+        'inputTipo': 'Selecione',
+        'inputCategoria': 'Selecione',
+        'inputDescricao': 'Inserir descrição',
+        'customDate': '' // Se desejar limpar a data
+    };
+
+    function clearData(...elementIds) {
+        elementIds.forEach(elementId => {
+            const element = document.getElementById(elementId);
+            if (element) {
+                // Armazena o valor original antes de limpar
+                originalValues[elementId] = element.value;
+
+                // Atribui o valor do espaço reservado antes de usá-lo
+                originalPlaceholders[elementId] = element.placeholder;
+
+                // Limpa o valor e define o espaço reservado de volta para o valor original
+                element.value = '';
+                element.placeholder = originalPlaceholders[elementId];
+
+                // Ativa o elemento
+                element.disabled = false;
+            }
+        });
+    }
+
+    // Limpa os dados para vários campos de entrada
+    clearData('inputValorTransacao', 'inputPerfil', 'inputTipo', 'inputCategoria', 'inputDescricao', 'customDate');
+
+    // Desmarca todos os itens de compartilhamento
+    const checkItems = document.querySelectorAll('.check-item');
+    checkItems.forEach(item => {
+        item.checked = false;
+    });
+
+    // Redefine o estado do checkbox recorrente
+    const inputRecorrente = document.getElementById('inputRecorrente');
+    if (inputRecorrente) {
+        inputRecorrente.checked = false;
+    }
+
+    // Ativa o checkbox "Todos"
+    const checkAll = document.getElementById('check-all');
+    if (checkAll) {
+        checkAll.checked = false;
+        checkAll.disabled = true;
+    }
+    const recorrenteOptions = document.querySelector('.recorrente-options');
+    if (recorrenteOptions) {
+        recorrenteOptions.style.display = 'none';
+    }
+}
